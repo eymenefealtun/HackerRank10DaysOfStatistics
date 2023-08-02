60b86f1c815dea47bbac97fbc5c19c816ceec897
eymenefealtun <eymenefealtun@hotmail.com>
2023-08-02 22:43:34 +0300

    Day-05 Binomial Distribution 1 added

---
+++ Day-05/BinomialDistribution1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day_05
+{
+    internal class BinomialDistribution1
+    {
+        public static void Execute()
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            string[] input = Console.ReadLine().Split(' ');
+
+            float p = (float)(Convert.ToDouble(input[0]) / (Convert.ToDouble(input[0]) + Convert.ToDouble(input[1])));
+            int n = 6;
+
+            double result = Binomial(3, n, p) + Binomial(4, n, p) + Binomial(5, n, p) + Binomial(6, n, p);
+
+            Console.WriteLine(Math.Round(result, 3));
+            Console.ReadLine();
+        }
+
+        static int Factorial(double n)
+        {
+            if (n < 2)
+                return 1;
+            return ((int)(Factorial(n - 1) * n));
+        }
+
+        static double Binomial(double x, double n, double p)
+        {
+            double f = Factorial(n) / (Factorial(n - x) * Factorial(x));
+
+            return (f * Math.Pow(p, x) * Math.Pow((1 - p), (n - x)));
+        }
+    }
+}
---
+++ Day-05/Executer.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Day_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinomialDistribution1.Execute();            
+        }
+    }
+}
