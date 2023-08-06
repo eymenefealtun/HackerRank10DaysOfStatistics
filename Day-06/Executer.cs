e1f73f2cc98e0c36b3572b6161cacc98032b8331
eymenefealtun <eymenefealtun@hotmail.com>
2023-08-06 22:51:15 +0300

    Poisson Distribution 2 added

--- Day-06/Executer.cs
+++ Day-06/Executer.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
-namespace Day_06
+﻿namespace Day_06
 {
     internal class Executer
     {
         static void Main(string[] args)
         {
-            PoissonDistribution1.Execute();
+            //PoissonDistribution1.Execute();
+            PoissonDistribution2.Execute();
         }
     }
 }
---
+++ Day-06/PoissonDistribution2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day_06
+{
+    internal class PoissonDistribution2
+    {
+        internal static void Execute( )
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+
+            double xA = 0.88 + Math.Pow(0.88, 2); ;
+            double dailyCostOfA = 160 + (40 * xA);
+
+            double xB = 1.55 + Math.Pow(1.55, 2);
+            double dailyCostOfB = 128 + (40 * xB);
+
+            Console.WriteLine(Math.Round(dailyCostOfA, 3));
+            Console.WriteLine(Math.Round(dailyCostOfB, 3));
+            Console.ReadLine();
+        }
+    }
+}
